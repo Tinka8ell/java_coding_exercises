58bd49f996484e420351c80c2844cb38383fa331
Mark Ingamells <mark.ingamells@gmail.com>
2022-03-14 15:46:55 +0000

    First commit - got capitalizeWord working and passing tests. This assumes that there are no preceding non-word characters in the provided string before the first word character. It should also cope with empty strings.

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,11 +1,14 @@
 package com.techreturners.exercise001;
 
 import java.util.List;
+import java.util.Locale;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
-        return "";
+        // As this is a simple test,
+        // we assume that the first character of the string (word)
+        // is the first character of the word that requires being made uppercase
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
