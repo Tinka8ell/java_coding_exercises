aedea48fb47f8ae9492cebb9fcc73af580772f25
Mark Ingamells <mark.ingamells@gmail.com>
2022-03-15 10:28:57 +0000

    Completed Exercise005

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -1,9 +1,17 @@
 package com.techreturners.exercise005;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Exercise005 {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        return input
+                .toLowerCase() // have to do it sometime, so bit the bullet now and use built-in function
+                .chars() // get it into a stream
+                .filter(a -> a >= 'a' && a <= 'z') // we are told it's ascii chars, so can limit it with this
+                .distinct() // leave one of each distinct letter
+                .count() >= 26; // check if we have all 26 (should never be > 26, but just in case!)
     }
 
 }
--- src/test/java/com/techreturners/exercise005/Exercise005Test.java
+++ src/test/java/com/techreturners/exercise005/Exercise005Test.java
@@ -21,52 +21,77 @@ public class Exercise005Test {
         assertFalse(ex005.isPangram(""));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkPerfectLowerCaseSentenceIsAPangram() {
         assertTrue(ex005.isPangram("abcdefghijklmnopqrstuvwxyz"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkPerfectUpperCaseSentenceIsAPangram() {
         assertTrue(ex005.isPangram("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkSentenceWithLowerCaseIsAPangram() {
         assertTrue(ex005.isPangram("the quick brown fox jumps over the lazy dog"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkSentenceWithMixedUpperAndLowerCaseIsAPangram() {
         assertTrue(ex005.isPangram("The Five boXing wiZards Jump QuicklY"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkSentenceWithMissingLetterXIsNotAPangram() {
         assertFalse(ex005.isPangram("the quick brown fo jumps over the lazy dog"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkSentenceWithMissingLetterHIsNotAPangram() {
         assertFalse(ex005.isPangram("five boxing wizards jump quickly at it"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkSentenceWithUnderscoresIsAPangram() {
         assertTrue(ex005.isPangram("the_quick_brown_fox_jumps_over_the_lazy_dog"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkSentenceWithNumbersIsAPangram() {
         assertTrue(ex005.isPangram("the 1 quick brown fox jumps over the 2 lazy dogs"));
+    }
+
+    @Test
+    public void checkExamplesFromWebsite() {
+        /*
+           https://pangrampangram.com/blogs/journal/best-pangrams-a-tool-for-every-graphic-designers
+           Using the samples from this page ...
+         */
+        String[] samples = {
+                "Two driven jocks help fax my big quiz.",
+                "Fickle jinx bog dwarves spy math quiz.",
+                "Public junk dwarves hug my quartz fox.",
+                "Quick fox jumps nightly above wizard.",
+                "Five quacking zephyrs jolt my wax bed.",
+                "The five boxing wizards jump quickly.",
+                "Pack my box with five dozen liquor jugs.",
+                "The quick brown fox jumps over the lazy dog.",
+                "When zombies arrive, quickly fax judge Pat.",
+                "Woven silk pyjamas exchanged for blue quartz.",
+                "The quick onyx goblin jumps over the lazy dwarf.",
+                "Foxy diva Jennifer Lopez wasn’t baking my quiche.",
+                "My girl wove six dozen plaid jackets before she quit.",
+                "Grumpy wizards make a toxic brew for the jovial queen.",
+                "A quivering Texas zombie fought republic linked jewelry.",
+                "All questions asked by five watched experts amaze the judge.",
+                "Back in June we delivered oxygen equipment of the same size.",
+                "The wizard quickly jinxed the gnomes before they vaporized.",
+                "We promptly judged antique ivory buckles for the next prize.",
+                "Jim quickly realized that the beautiful gowns are expensive."
+        };
+        for (String sentence : samples) {
+            assertTrue(ex005.isPangram(sentence));
+        }
     }
 
 }
