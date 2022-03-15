d5c2e503b8c5c94d1db565769a52f4daa6985f17
Mark Ingamells <mark.ingamells@gmail.com>
2022-03-15 07:48:21 +0000

    Completed Exercise003 - is this really that simple?

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -1,13 +1,42 @@
 package com.techreturners.exercise003;
 
+/**
+ * KISS - Keep it simple stupid!
+ * Is this really this simple?
+ * Am I missing something?
+ */
 public class Exercise003 {
+    /**
+     * Look up table to convert names to id:
+     * - Pistachio: 0
+     * - Raspberry Ripple: 1
+     * - Vanilla: 2
+     * - Mint Chocolate Chip: 3
+     * - Chocolate: 4
+     * - Mango Sorbet: 5
+     */
+    private static final String[] lookUpTable = {
+            "Pistachio",
+            "Raspberry Ripple",
+            "Vanilla",
+            "Mint Chocolate Chip",
+            "Chocolate",
+            "Mango Sorbet"
+    };
 
     int getIceCreamCode(String iceCreamFlavour) {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        int index = -1;
+        for (int i = 0; i < lookUpTable.length; i++) {
+            if (iceCreamFlavour.equalsIgnoreCase(lookUpTable[i])){
+                index = i;
+                break;
+            }
+        }
+        return index;
     }
 
     String[] iceCreamFlavours() {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        return lookUpTable;
     }
 
 }
